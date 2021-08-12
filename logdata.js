75c49257fbe48898887935179a01778ef92edd89
Manu Devos <manudevos@gmail.com>
2021-08-13 00:31:55 +0200

    Add first JS file

---
+++ logdata.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "John Doe",
+    age: 26,
+    country: "Belgium"
+}
+
+function logDate(){
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
