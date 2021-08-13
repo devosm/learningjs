ed428d77a5844c95a1d36eae63c2fef181ca2e51
Manu Devos <manudevos@gmail.com>
2021-08-13 11:40:25 +0200

    Minor changes

--- forloop.js
+++ forloop.js
@@ -1,5 +1,5 @@
 let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
 
-for (let i = 0; i < largeCountries.length; i += 1){
-    console.log(largeCountries[i])
+for (let i = 0; i < largeCountries.length; i ++){
+    console.log("- " + largeCountries[i])
 }
