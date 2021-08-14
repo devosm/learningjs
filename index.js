701bfd82aa9bb18ec84f4865381e82121fc18a4c
Manu Devos <manudevos@gmail.com>
2021-08-14 20:43:22 +0200

    Further changes to test

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let scripts = ["forloop","logdata","logicaloperator","math","recapaftergame","shift"]
 let scriptsEl = document.getElementById("scripts-el")
+let plainTextEl = document.getElementById("plainText-el")
 
 //Adds scripts array to dropdown in index.html
 for(i = 0; i < scripts.length; i ++){
     let allScripts = scripts[i]
     scriptsEl.innerHTML += "<option value=" + "\"" + allScripts + "\"" + ">" + allScripts + "</option>"
-    console.log(allScripts)
 }
+
+var imported = document.createElement('script');
+imported.src = './Learned Scripts/forloop.js';
+plainTextEl.innerText = imported
